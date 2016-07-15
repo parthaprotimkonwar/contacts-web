6ba6211f07d67a507ae361ea6bd3a9ac47671767
Partha Protim Konwar <parthaprotimkonwar@gmail.com>
2016-07-15 10:18:33 +0530

    Vendor Integration completed

--- js/controllers/vendor_controller.js
+++ js/controllers/vendor_controller.js
@@ -17,7 +17,55 @@ myApp.controller('vendorController', ['$scope', '$http', '$location', 'commonSer
 
     $scope.addAddress = function() {
         console.log('go go go');
-        $location.path('/contacts/vendor/address/edit');
+        var data = {"cityId" : $scope.user.city_Id, "userType" : "VENDORS", "name" : $scope.user.name, "email" : $scope.user.email, "mobile" : $scope.user.mobile};
+        console.log(data);
 
     }
+
+    //List all Cities
+    $scope.listCities = function() {
+        $scope.listCitiesPromise = commonServices.sendHttpRequest("http://localhost:9000/admin/list/cities",CONSTANTS.GET_METHOD,null);
+        $scope.listCitiesPromise.success(function (data, status, headers, config) {
+            console.log('Got back a response');
+            console.log(data);
+            $scope.cityList = data.data;
+        }).error(function (data, status, headers, config) {
+            console.log('AWS DOWN');
+        });
+    }
+
+    $scope.listCities();
+
+    //Add User
+    $scope.addUser = function() {
+        var data = {"cityId" : $scope.user.city_Id, "userType" : "VENDORS", "name" : $scope.user.name, "email" : $scope.user.email, "mobile" : $scope.user.mobile, "status" : "ACTIVE"};
+        $scope.addUserPromise = commonServices.sendHttpRequest("http://localhost:9000/admin/onboard/user",CONSTANTS.POST_METHOD,data);
+        $scope.addUserPromise.success(function (data, status, headers, config) {
+            console.log('Got back a response');
+            console.log(data);
+
+            $scope.listUsers();
+            $scope.operationStatus = CONSTANTS.SUCCESS;
+            $scope.message = 'Data push is successful';
+            $scope.user = {};
+        }).error(function (data, status, headers, config) {
+            console.log('AWS DOWN');
+            $scope.operationStatus = CONSTANTS.FAILURE;
+            $scope.message = 'Unable to push data';
+        });
+    }
+
+    //List all Users
+    $scope.listUsers = function() {
+        $scope.listUsersPromise = commonServices.sendHttpRequest("http://localhost:9000/admin/list/users",CONSTANTS.GET_METHOD,null);
+        $scope.listUsersPromise.success(function (data, status, headers, config) {
+            console.log('Got back a response');
+            console.log(data);
+            $scope.userList = data.data;
+        }).error(function (data, status, headers, config) {
+            console.log('AWS DOWN');
+        });
+    }
+
+    $scope.listUsers();
 }]);
